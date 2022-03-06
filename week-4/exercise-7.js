4a69ad78ee33206471db2a1c137ed99d0f014d1b
naga-reddy27 <mns.reddy07@gmail.com>
2022-03-06 22:22:41 +0000

    details added

--- week-4/exercise-7.js
+++ week-4/exercise-7.js
@@ -1,13 +1,12 @@
 function buttonClick() {
-    const input = document.getElementById("text");
+    const input = document.getElementById("text_no");
     const value = input.value;
-
-    const valueDiv = document.getElementById("value");
+    const valueDiv = document.getElementById("button_no");
     valueDiv.innerHTML = value;
 }
 
 function load() {
-    const text = document.getElementById("text");
+    const text = document.getElementById("text_no");
     text.addEventListener("keyup",buttonClick )
 }
 
